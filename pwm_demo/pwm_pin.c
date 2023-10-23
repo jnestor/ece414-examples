f91b4d1178be3aa2484aa4236cfb3d06c061a37e
nestorj <nestorj@lafayette.edu>
2023-10-23 13:28:19 -0400

    updated/corrected ic_inter and ic_pio RPM measurments

--- pwm_demo/pwm_pin.c
+++ pwm_demo/pwm_pin.c
@@ -1,5 +1,5 @@
 /*
- * pwm24 - configure GPIO24 as a PWM output
+ * pwm-pin - configure GPIO24 as a PWM output
  * and provide a function to change the
  * duty cycle
  *
@@ -15,10 +15,14 @@
 static uint slice_num;
 
 // Initialize pin 25 as a basic PWM output
+// By setting the pwm clock divider to divide 125MHz/1.9375
+// we will count at 63.23 MHz (15.8ns) 
+// By seetting the wrap at 0xfff output waveform will have
+// a frequency of 965.65 Hz (1.035ms)
 void pwm_pin_init(uint16_t level) {
     gpio_set_function(PWM_PIN, GPIO_FUNC_PWM);
     slice_num = pwm_gpio_to_slice_num(PWM_PIN);
-    pwm_set_clkdiv(slice_num, 1.9375);
+    pwm_set_clkdiv(slice_num, 1.9375);  
     pwm_set_wrap(slice_num, 0xffff);
     pwm_set_chan_level(slice_num, PWM_CHAN_A, level);
     pwm_set_enabled(slice_num, true);
